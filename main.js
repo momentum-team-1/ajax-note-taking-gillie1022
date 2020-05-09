753437515695ae8a6098fbaf96bd8412cc7527b3
gillie1022 <dave.gillie@icloud.com>
2020-05-09 11:33:06 -0400

    post works

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ noteForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let noteTitleInput = document.querySelector("#title-input");
   let titleText = noteTitleInput.value;
-  createNewNote(titleText);
+  let noteInput = document.querySelector("#note-text")
+  let noteText = noteInput.value
+  createNewNote(titleText, noteText);
 });
+
+function createNewNote(title, note){
+  fetch("http://localhost:3000/notes/", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({tItem: title, nItem: note, created: moment().format("MMMM Do YYYY, h:mm:ss a")})
+  })
+  }
